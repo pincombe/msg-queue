108bd76fd907c7a665ffdc8564a6c8dc4eae29e6
pincombe <pincombe@gmail.com>
2013-03-11 16:48:37 +0000

    Fixed Whitespace issues

--- src/MsgQueue/Connection.php
+++ src/MsgQueue/Connection.php
@@ -4,52 +4,48 @@ namespace MsgQueue;
 
 class Connection
 {
-	public static $connection;
+    public static $connection;
 
-	public function __construct($hostname = '127.0.0.1')
-	{
+    public function __construct($hostname = '127.0.0.1')
+    {
         if (empty(self::$connection)) {
-
             self::$connection = new \Pheanstalk\Pheanstalk($hostname);
-
-			$listening = self::$connection->getConnection()->isServiceListening();
-			if (!$listening) {
-				throw new \Exception('Message queue server is not available');
-			}
-
+            $listening = self::$connection->getConnection()->isServiceListening();
+            if (!$listening) {
+                throw new \Exception('Message queue server is not available');
+            }
         }
 
         return self::$connection;
-	}
+    }
 
-	public function put($tube, $data)
-	{
-		self::$connection->useTube($tube)->put($data);
-	}
+    public function put($tube, $data)
+    {
+        self::$connection->useTube($tube)->put($data);
+    }
 
-	public function bury($job)
-	{
-		self::$connection->bury($job);
-	}
+    public function bury($job)
+    {
+        self::$connection->bury($job);
+    }
 
-	public function kick($tube, $job_count = 1)
-	{
-		return self::$connection->useTube($tube)->kick($job_count);
-	}
+    public function kick($tube, $job_count = 1)
+    {
+    	return self::$connection->useTube($tube)->kick($job_count);
+    }
 
-	public function reserve($tube)
-	{
-		return self::$connection->watch($tube)->ignore('default')->reserve();
-	}
+    public function reserve($tube)
+    {
+        return self::$connection->watch($tube)->ignore('default')->reserve();
+    }
 
-	public function delete($job)
-	{
-		self::$connection->delete($job);
-	}
+    public function delete($job)
+    {
+        self::$connection->delete($job);
+    }
 
-	public function statsTube($name)
-	{
-		return self::$connection->statsTube($name);
-	}
-
+    public function statsTube($name)
+    {
+    	return self::$connection->statsTube($name);
+    }
 }
